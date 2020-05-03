767d1ca0cf255a1c482bb10a4ae4fd6c15b7416e
samemaru <66pistols@gmail.com>
2020-05-04 00:50:43 +0900

    I-12

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // constructor(props) {super(props); は、定型文だと思っていい。
+    this.state = {name:'はるた'};
+    
+  }
+
+  handleClick(name){
+    this.setState({name: name});
+  }
+
+
+
   render() {
     return (
       <div>
-        <h1>こんにちは！はるたさん！Sample</h1>
-        <button>はるた</button>
-        <button>あきお</button>
+        <h1>こんにちは！{this.state.name}さん！</h1>
+        <button onClick={()=>{this.handleClick('はるた')}}>はるた</button>
+        <button onClick={()=>{this.handleClick('あきお')}}>あきお</button>
       </div>
     );
   }
