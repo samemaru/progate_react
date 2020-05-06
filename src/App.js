69702aafb6365fdacd544d82f47cd52012f1e49b
samemaru <66pistols@gmail.com>
2020-05-06 22:11:54 +0900

    add count-up-app

--- src/App.js
+++ src/App.js
@@ -1,55 +1,62 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {count:0};
+    
+  }
+  
+  handleClick(){
+    this.setState({count : this.state.count + 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.count}
+          
+        </h1>
+        <button onClick={()=>{this.handleClick()}}>+</button>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+/* I-1~12 : 切り替えボタン
+
+
+class App extends React.Component {
 
   constructor(props) {
     super(props);
     // constructor(props) {super(props); は、定型文だと思っていい。
     this.state = {name:'はるた'};
-    this.state = {count: 0};
+    
   }
 
   handleClick(name){
     this.setState({name: name});
   }
 
-
-
   render() {
     return (
+      // JSXは1つの要素しか入れられない！
       <div>
-        <h1>こんにちは！あああ{this.state.name}さん！</h1>
+        <h1>こんにちは！{this.state.name}さん！</h1>
         <button onClick={()=>{this.handleClick('はるた')}}>はるた</button>
         <button onClick={()=>{this.handleClick('あきお')}}>あきお</button>
       </div>
-
-      <div>
-      <h1>
-        {this.state.count}
-      </h1>
-      {/* <button>タグ内でonClickイベントを追加してください */}
-      <button onClick ={() => {this.handleClick()}}>+</button>
-
-</div>
-
     );
   }
 }
+*/
 
-// 1-12 countup
-handleClick(){
-  this.setState({count: this.state.count +1});
-  }
-
-  
-  // handleClickメソッドを定義してください
- 
-  
-  render() {
-    return (
-
-    );
-  }
-}
-
-export default App;
