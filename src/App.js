f77d530d00ade71273a5ea3ddea1e1169d06867f
samemaru <66pistols@gmail.com>
2020-05-17 09:21:35 +0900

    copy 3-4 コンポーネントを作ろう

--- src/App.js
+++ src/App.js
@@ -1,127 +1,14 @@
 import React from 'react';
-import Language from './Language';
-import Lesson from './Lesson';
+import Main from './Main';
 
-class Lesson extends React.Component{
-  render(){
-    return(
-        <div>
-          <main />
-
-        </div>
-    )
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Main />
+      </div>
+    );
   }
 }
 
 export default App;
-
-/*
-class App extends React.Component {
-  render() {
-    // 指定されたコードを貼り付けてください
-    const languageList = [
-      {
-        name: 'HTML & CSS',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
-      },
-      {
-        name: 'JavaScript',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
-      },
-      {
-        name: 'React',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
-      },
-      {
-        name: 'Ruby',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg'
-      },
-      {
-        name: 'Ruby on Rails',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg'
-      },
-      {
-        name: 'Python',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg'
-      }
-    ];
-  
-    return (
-      <div>
-        <h1>言語一覧</h1>
-        <div className='language'>
-
-          {/* mapメソッドを用いて、Languageコンポーネントを表示 }
-          {languageList.map((languageItem) => {
-            return (
-              // Languageコンポーネントを呼び出し、その中でpropsを渡す
-              <Language 
-              name = {languageItem.name}
-              image = {languageItem.image} />
-              
-            )
-          })}
-          
-        </div>
-      </div>
-    );
-  }
-}
-
-
-*/
-
-
-
-
-
-/* カウントアップボタン
-handleClick(){
-    this.setState({count : this.state.count + 1});
-  }
-
-render() {
-    return (
-      <div>
-        <h1>
-
-        
-          {this.state.count}
-          
-        </h1>
-        <button onClick={()=>{this.handleClick()}}>+</button>
-      
-      </div>
-    );
-  }
-
-*/
-
-/* I-1~12 : 切り替えボタン
-
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    // constructor(props) {super(props); は、定型文だと思っていい。
-    this.state = {name:'はるた'};
-    
-  }
-
-  handleClick(name){
-    this.setState({name: name});
-  }
-
-  render() {
-    return (
-      // JSXは1つの要素しか入れられない！
-      <div>
-        <h1>こんにちは！{this.state.name}さん！</h1>
-        <button onClick={()=>{this.handleClick('はるた')}}>はるた</button>
-        <button onClick={()=>{this.handleClick('あきお')}}>あきお</button>
-      </div>
-    );
-  }
-}
-*/
--- src/Main.js
+++ src/Main.js
@@ -1,51 +1,52 @@
-import react from 'react';
+import React from 'react';
+import Lesson from './Lesson';
 
-class Main {
-    render(){
-        const lessonList = [
-            {
-              name: 'HTML & CSS',
-              image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg',
-              introduction: 'WEBページはHTML、CSSという言語によってその見た目が作られています。 実際にWEBページを作りながら学んでみましょう！',
-            },
-            {
-              name: 'Sass',
-              image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/sass.svg',
-              introduction: 'SassはCSSをより便利に効率的にするための言語です。',
-            },
-            {
-              name: 'JavaScript',
-              image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg',
-              introduction: 'JavaScriptはフロントエンドだけでなく、サーバーサイドまで広い可能性を持つプログラミング言語です。',
-            },
-            {
-              name: 'React',
-              image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg',
-              introduction: 'ReactはHTMLのように、サイトの見た目をつくることができるJavaScriptのライブラリです。',
-            },
-          ];
-          
-          return(
-            <div className='main-wrapper'>
-                <div className='main'>
-                    <div className='copy-container'>
-                        <h1>Hello,World</h1>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-            </div>
-
-
-          )
-    }
+class Main extends React.Component {
+  render() {
+    const lessonList = [
+      {
+        name: 'HTML & CSS',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg',
+      },
+      {
+        name: 'Sass',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/sass.svg',
+      },
+      {
+        name: 'JavaScript',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg',
+      },
+      {
+        name: 'React',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg',
+      },
+    ];
+    
+    return (
+      <div className='main-wrapper'>
+        <div className='main'>
+          <div className='copy-container'>
+            <h1>Hello, World.</h1>
+            <h2>プログラミングの世界へようこそ！</h2>
+          </div>
+          <div className='lesson-container'>
+            <h3 className='section-title'>学べるレッスン</h3>
+            {/* lessonListに対して、mapメソッドを用いてください */}
+             {lessonList.map((lessonItem)=>{
+               return(
+               <Lesson
+                  name={lessonItem.name}
+                  image={lessonItem.image}
+                />
+               );
+             }
+             )}
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
-
-
 
 export default Main;
