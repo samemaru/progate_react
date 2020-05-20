7a838edcfb6ba98c9f87b8e464f0904d5d594594
samemaru <66pistols@gmail.com>
2020-05-20 20:17:04 +0900

    3-9 modal

--- src/component/Main.js
+++ src/component/Main.js
@@ -7,21 +7,25 @@ class Main extends React.Component {
       {
         name: 'HTML & CSS',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg',
+        introduction: 'WEBページはHTML、CSSという言語によってその見た目が作られています。 実際にWEBページを作りながら学んでみましょう！',
       },
       {
         name: 'Sass',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/sass.svg',
+        introduction: 'SassはCSSをより便利に効率的にするための言語です。',
       },
       {
         name: 'JavaScript',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg',
+        introduction: 'JavaScriptはフロントエンドだけでなく、サーバーサイドまで広い可能性を持つプログラミング言語です。',
       },
       {
         name: 'React',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg',
+        introduction: 'ReactはHTMLのように、サイトの見た目をつくることができるJavaScriptのライブラリです。',
       },
     ];
-    
+
     return (
       <div className='main-wrapper'>
         <div className='main'>
@@ -30,18 +34,18 @@ class Main extends React.Component {
             <h2>プログラミングの世界へようこそ！</h2>
           </div>
           <div className='lesson-container'>
-            <h3 className='section-title'>学べるレッスン</h3>
-            {/* lessonListに対して、mapメソッドを用いてください */}
-             {lessonList.map((lessonItem)=>{
-               return(
-               <Lesson
+            <h3>学べるレッスン</h3>
+            {lessonList.map((lessonItem) => {
+              return (
+                <Lesson
                   name={lessonItem.name}
                   image={lessonItem.image}
+                  // introductionをpropsで渡してください
+                  introduction={lessonItem.introduction}
+                  
                 />
-               );
-             }
-             )}
-            
+              );
+            })}
           </div>
         </div>
       </div>
