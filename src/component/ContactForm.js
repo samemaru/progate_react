c4d65b1b592e253a6588f2a611fc470559d23ac7
samemaru <66pistols@gmail.com>
2020-06-07 22:51:11 +0900

    4-3 送信メッセージの作成

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,21 +1,45 @@
 import React from 'react';
 
 class ContactForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isSubmitted : false,
+    };
+  }
+
   render(){
-    return(
-      <div className='contact-form'>
+    let contactForm;
+
+    if(this.state.isSubmitted){
+      contactForm = (
+        <div className='contact-submit-massage'>
+          送信完了
+        </div>
+      );
+    }else{
+      contactForm = (
         <form>
           <p>メールアドレス</p>
           <input />
 
           <p>お問い合わせ内容</p>
-          <textarea></textarea>
-          
+          <textarea />
+
           <input 
             type='submit'
             value='送信'
             />
         </form>
+      );
+    }
+
+
+
+
+    return(
+      <div className='contact-form'>
+      {contactForm}
       </div>
     );
 
